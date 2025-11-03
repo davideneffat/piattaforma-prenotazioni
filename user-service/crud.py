3240f90f33a7762e2497398c04862ab0d71fba8c
davideneffat <davideneffat@gmail.com>
2025-11-03 17:21:23 +0100

    added login api with jwt token

--- user-service/crud.py
+++ user-service/crud.py
@@ -2,13 +2,21 @@ from sqlalchemy.orm import Session
 from models import User
 from passlib.context import CryptContext
 from schemas import UserCreate
+from auth import verify_password, get_password_hash
 
-pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
 
-def create_user(db: Session, user: UserCreate):
-    hashed = pwd_context.hash(user.password)
-    db_user = User(username=user.username, email=user.email, hashed_password=hashed)
+def create_user(db: Session, user):
+    hashed_password = get_password_hash(user.password)
+    db_user = User(username=user.username, email=user.email, hashed_password=hashed_password)
     db.add(db_user)
     db.commit()
-    db.refresh(db_user) 
+    db.refresh(db_user)
     return db_user
+
+def authenticate_user(db: Session, username: str, password: str):
+    user = db.query(User).filter(User.username == username).first()
+    if not user:
+        return None
+    if not verify_password(password, user.hashed_password):
+        return None
+    return user
--- user-service/main.py
+++ user-service/main.py
@@ -1,10 +1,14 @@
-from fastapi import Depends, FastAPI
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
-from crud import create_user
 from database import SessionLocal
 from schemas import UserCreate
+from crud import create_user, authenticate_user
+from auth import create_access_token
 
 app = FastAPI()
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
 def get_db():
     db = SessionLocal()
@@ -16,3 +20,12 @@ def get_db():
 @app.post("/register")
 def register(user: UserCreate, db: Session = Depends(get_db)):
     return create_user(db, user)
+
+@app.post("/login")
+def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    user = authenticate_user(db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials")
+    
+    access_token = create_access_token(data={"sub": user.username})
+    return {"access_token": access_token, "token_type": "bearer"}
